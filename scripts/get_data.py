34dd9c2732c70775158d9917f97f91009257ca38
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2024-08-12 19:44:07 -0400

    Add more extensions

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as allowedcolors
 
-extensions = ['uBlock Origin', "AdGuard AdBlocker"]
+extensions = ['uBlock Origin', "AdGuard AdBlocker", "Adblock Plus", "SponsorBlock", "Ghostery", "AdBlock for Firefox"]
 current_date = datetime.datetime.now().strftime("%d/%m/%Y")
 
 def dict_as_arr(dic):
